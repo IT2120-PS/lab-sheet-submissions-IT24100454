02e26d1227015f4bef6667941fd1dceec774559a
Chandrasiri P G S K <it24100454@my.sliit.lk>
2025-10-01 13:05:01 +0530

    Add files via upload

---
+++ Lab Sheet 09/IT24100454 (2).R
@@ -0,0 +1,8 @@
+setwd("C://Users//HP//Desktop//IT24100454")
+getwd()
+set.seed(123)
+bake_time<-rnorm(25,mean=45,sd=2)
+print(bake_time)
+#ii
+t.test(bake_time,mu=46,alternative = "less",conf.level = 0.95)
+print(t.test)
